7cd7542031377fd4443e9fb6bee9ac386937b87c
prodgaraj@yandex.ru <prodgaraj@yandex.ru>
2020-12-25 00:25:58 +0300

    Add Main()

--- University/Program.cs
+++ University/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using University.Data;
+using University.Data.Models;
 
-namespace University1
+namespace University
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var context = new UniversityContext())
+            {
+                var student = new Student
+                {
+                    Name = "Вася",
+                    PhoneNumber = "9991234567",
+                    RegisteredOn = DateTime.Now
+                };
+
+                context.Students.Add(student);
+                context.Courses.Add(new Course
+                {
+                    Name = "EF CORE",
+                    Price = 0,
+                    Description = "Курс о разработке приложений работать работающих с БД",
+                    StartDate = DateTime.Today,
+                    EndDate = DateTime.Today.AddDays(30)
+                });
+
+                context.SaveChanges();
+            }
         }
     }
 }
